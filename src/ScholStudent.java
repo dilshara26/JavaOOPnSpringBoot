9bff3d483b0d781b399acc7f7b89d0301fe9b045
Dilshara <dilsharahasanka@gmail.com>
2025-03-10 09:29:25 +0530

    Initial Commit

--- src/ScholStudent.java
+++
@@ -1,7 +0,0 @@
-public class ScholStudent extends Student{
-    String scholType;
-    ScholStudent(String name, String email, int studentID, String scholType) {
-        super(name, email, studentID);
-        this.scholType = scholType;
-    }
-}
